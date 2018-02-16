27c74be72a5018c1a2ff078958293a46eafb03c5
ADJM <adejesus@ec-lyon.fr>
2018-02-16 15:12:31 +0100

    Hotfix collectData.js

--- collect-data/collectData.js
+++ collect-data/collectData.js
@@ -9,6 +9,56 @@ const async = require('async'); //to make async operations
 const fs = require('fs');
 const logger = require('../utils/logger')('tmdb');
 
+let searchPeople = (req, callback) => {
+	superagent.get(`${config.tmdb.website_url}/search/person`).query(req).end((err, response) => {
+		if (err || typeof response === 'undefined') return callback(err.message);
+		callback(null, response);
+	});
+}
+
+let getPeople = (id, req, callback) => {
+	superagent.get(`${config.tmdb.website_url}/person/${id}`).query(req).end((err, res) => {
+		if (err || typeof res === 'undefined') return callback(err.message);
+		callback(null, res);
+	});
+}
+
+let getPeopleCredits = (id, req, callback) => {
+	superagent.get(`${config.tmdb.website_url}/person/${id}/movie_credits`).query(req).end((err, credits) => {
+		if (err || typeof credits === 'undefined') return callback(err.message);
+		callback(null, credits);
+	});
+}
+
+let researchGoodResult = (results, movieName, request, callback) => {
+	var goodResult;
+	async.eachSeries(results, (result, asyncIterator) => {
+		let person_id = result.id;
+
+		getPeopleCredits(person_id, request, (err, credits) => {
+			if (err) return asyncIterator(err);
+
+			let movies = credits.body;
+
+			for (var key in movies) {
+				if (movies.hasOwnProperty(key) && (key ==='crew' || key === 'cast')) {
+					let search = movies[key].filter( movie => {
+						return movie.original_title === movieName;
+					});
+					if (search.length !== 0) {
+						goodResult = result;
+						return asyncIterator('Done');
+					}
+				}
+			}
+			asyncIterator();
+		})
+	}, message => {
+		if (message === 'Done') return callback(null, goodResult);
+		callback(message);
+	})
+}
+
 /*
 Beginning of the script
 */
@@ -39,6 +89,8 @@ fs.readFile('./data/nominees.txt', 'latin1', (err, data) => {
 
 		async.eachSeries(nominees, (nominee, asyncIterator) => {
 
+			console.log(nominee);
+
 			setTimeout( () => {
 
 				let request = {
@@ -49,64 +101,70 @@ fs.readFile('./data/nominees.txt', 'latin1', (err, data) => {
 					include_adult: 'false'
 				};
 
-				superagent.get(`${config.tmdb.website_url}/search/person`).query(request).end((err, response) => {
-					if (err || typeof response === 'undefined') return asyncIterator(err.message);
+				searchPeople(request, (err, response) => {
+					if (err) return asyncIterator(err);
 
 					if (response.body.results.length === 0) return asyncIterator();
 
-					let searchNominee = people.findIndex( obj => {
-						return obj.name === response.body.results[0].name;
-					});
+					researchGoodResult(response.body.results, movie, request, (err, goodResult) => {
+						if (err) return asyncIterator(err);
 
-					if (searchNominee !== -1) {
-						people[searchNominee].nomination.push({
-							movie: movie,
-							award: award
+						if (!goodResult) return asyncIterator();
+
+						let searchNominee = people.findIndex( obj => {
+							return obj.name === goodResult.name;
 						});
-						asyncIterator();
-					}
-					else {
-						let person_id = response.body.results[0].id;
 
-						superagent.get(`${config.tmdb.website_url}/person/${person_id}`).query(request).end((err, res) => {
-							if (err || typeof res === 'undefined') return asyncIterator(err.message);
-
-							let person = res.body;
-
-							superagent.get(`${config.tmdb.website_url}/person/${person_id}/movie_credits`).query(request).end((err, credits) => {
-								if (err || typeof credits === 'undefined') return asyncIterator(err.message);
-
-								let movies = credits.body;
-
-								let movie_credits = {};
-
-								movie_credits.cast = movies.cast.filter( movie => {
-									let date = new Date(movie.release_date);
-									return (Number(date.getUTCFullYear()) + 5 >= 2017 && Number(date.getUTCFullYear()) < 2018) && typeof movie.character !== 'undefined' && movie.character.indexOf('Himself') === -1;
-								});
-
-								movie_credits.crew = movies.crew.filter( movie => {
-									let date = new Date(movie.release_date);
-									return Number(date.getUTCFullYear()) + 5 >= 2017 && Number(date.getUTCFullYear()) < 2018 && typeof movie.job !== 'undefined' && movie.job.indexOf('Thanks') === -1;
-								});
-
-								let nomination = {
-									movie: movie,
-									award: award
-								};
-								let tmp_array = [];
-								tmp_array.push(nomination);
-
-								person.credits = movie_credits;
-								person.nomination = tmp_array;
-								delete person.also_known_as;
-
-								people.push(person);
-								asyncIterator();
-
+						if (searchNominee !== -1) {
+							people[searchNominee].nomination.push({
+								movie: movie,
+								award: award
 							});
-						});
-					}
+							asyncIterator();
+						}
+						else {
+							let person_id = goodResult.id;
+
+							getPeople(person_id, request, (err, res) => {
+								if (err) return asyncIterator(err);
+
+								let person = res.body;
+
+								getPeopleCredits(person_id, request, (err, credits) => {
+									if (err) return asyncIterator(err);
+
+									let movies = credits.body;
+
+									let movie_credits = {};
+
+									movie_credits.cast = movies.cast.filter( movie => {
+										let date = new Date(movie.release_date);
+										return (Number(date.getUTCFullYear()) + 5 >= 2017 && Number(date.getUTCFullYear()) < 2018) && typeof movie.character !== 'undefined' && movie.character.indexOf('Himself') === -1;
+									});
+
+									movie_credits.crew = movies.crew.filter( movie => {
+										let date = new Date(movie.release_date);
+										return Number(date.getUTCFullYear()) + 5 >= 2017 && Number(date.getUTCFullYear()) < 2018 && typeof movie.job !== 'undefined' && movie.job.indexOf('Thanks') === -1;
+									});
+
+									let nomination = {
+										movie: movie,
+										award: award
+									};
+									let tmp_array = [];
+									tmp_array.push(nomination);
+
+									person.credits = movie_credits;
+									person.nomination = tmp_array;
+									delete person.also_known_as;
+
+									people.push(person);
+									asyncIterator();
+
+								});
+							});
+						}
+					});
 				});
 			}, 500);
 		}, err => {
